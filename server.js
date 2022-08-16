aaeb477ca365702e20d18a9f937fde21402aa6b9
rushanthan03 <prushanthan@gmail.com>
2022-08-16 17:08:35 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express =require('express');
+const http = require('http');
+const port = 3000;
+let app = express();
+const server = http.createServer(app);
+
+server.listen(port, ()=>{
+    console.log("server is listening on port 3000");
+});
