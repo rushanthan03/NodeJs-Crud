5f7ee3787fbbd8d49efa80771f8d25da0a0eb406
rushanthan03 <prushanthan@gmail.com>
2022-08-27 11:32:26 +0530

    app js push

---
+++ app.js
@@ -0,0 +1,21 @@
+module.exports = function(app)
+{
+    //define function
+    app.use('/get',(req, res) =>{
+        res.status(200).send('succesfully');
+    })
+    app.use('/put',(req,res) => {
+        res.status(200).send('changed req');
+    })
+    app.use('/post',(req,res) => {
+        res.status(200).send('hello');
+    })
+    //undifine function
+    app.use((req, res)=>{
+        res.status(404).json({message:'404 - Not Found'});
+    })
+
+    app.use((err,req,res)=>{
+        res.status(500).json({message:'internal Server error',error:err});
+    })
+}
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express =require('express');
 const http = require('http');
+const appconfig= require('./app.js')
 const port = 3000;
 let app = express();
-const server = http.createServer(app);
 
+const server = http.createServer(app);
+appconfig(app);
 server.listen(port, ()=>{
     console.log("server is listening on port 3000");
 });
